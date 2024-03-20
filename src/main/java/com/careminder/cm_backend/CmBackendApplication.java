db9a72d7961743a4afbf5aa6ca1c50e7077c075d
이정민 <kyungheeyj45@ajou.ac.kr>
2024-03-21 02:48:43 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/careminder/cm_backend/CmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.careminder.cm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careminder/cm_backend/CmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careminder.cm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
